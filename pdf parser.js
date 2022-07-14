43bb29cd25b6358ef5381a4c5eab142c8e36c91c
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-07-14 10:25:38 +0530

    updated

--- pdf parser.js
+++ pdf parser.js
@@ -1,9 +1,21 @@
-const fs=require('fs')
-const pdfparse=require('pdf-parse')
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const pdfParse = require("pdf-parse");
 
-const pdffile = fs.readFileSync('Sample.pdf')
-//getting information
-pdfparse(pdffile).then(function(data){
-    const objdata=JSON.parse(data.text);
-    console.log(objdata);
-})
+const app = express();
+
+app.use("/", express.static("public"));
+app.use(fileUpload());
+
+app.post("/extract-text", (req, res) => {
+    if (!req.files && !req.files.pdfFile) {
+        res.status(400);
+        res.end();
+    }
+
+    pdfParse(req.files.pdfFile).then(result => {
+        res.send(result.text);
+    });
+});
+
+app.listen(3000);
